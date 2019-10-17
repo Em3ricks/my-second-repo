354ce1cd2f481d145e42b57d1b4e5c0e47d58549
Em3ricks <55283526+Em3ricks@users.noreply.github.com>
2019-10-17 14:05:35 +0200

    Delete index-scripts.js

--- build/js/index-scripts.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	var el = $('h1').eq(0);
-	var text = el.text();
-	var markup = '';
-	for (i = 0; i < text.length; i++) {
-		markup += '<span>' + text[i] + '</span>';
-	}
-	el.html(markup);
-});
